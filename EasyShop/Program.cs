3165fb8af26e55d3a8af2dfa0d1c96ba271c27c0
prafu <prafu@AARVI>
2024-11-22 14:20:47 +0530

    Add project files.

---
+++ EasyShop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using EasyShop.Models;
+using EasyShop.Services;
+
+namespace ShoppingBasket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dependency Injection: Register available items
+            List<IItem> availableItems = new List<IItem>
+            {
+                new Apple(),
+                new Banana(),
+                new Melon(),
+                new Lime()
+            };
+
+            // Inject dependencies into the Basket
+            Basket basket = new Basket(availableItems);
+
+            // Example basket
+            List<string> basketItems = new List<string> { "Apple", "Apple", "Banana", "Melon", "Melon", "Lime", "Lime", "Lime", "Lime" };
+
+            // Calculate total cost
+            decimal totalCost = basket.CalculateTotalCost(basketItems);
+            Console.WriteLine($"Total cost of the basket: £{totalCost:F2}");
+        }
+    }
+}
---
+++ EasyShop/Services/Basket.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using EasyShop.Models;
+
+namespace EasyShop.Services
+{
+    public class Basket
+    {
+        private readonly List<IItem> _items;
+
+        public Basket(List<IItem> items)
+        {
+            _items = items;
+        }
+
+        public decimal CalculateTotalCost(List<string> basketItems)
+        {
+            Dictionary<string, int> itemCounts = new Dictionary<string, int>();
+            foreach (var item in basketItems)
+            {
+                if (itemCounts.ContainsKey(item))
+                    itemCounts[item]++;
+                else
+                    itemCounts[item] = 1;
+            }
+
+            decimal totalCost = 0;
+            foreach (var item in _items)
+            {
+                if (itemCounts.ContainsKey(item.Name))
+                    totalCost += item.GetPrice(itemCounts[item.Name]);
+            }
+
+            return totalCost;
+        }
+    }
+}
